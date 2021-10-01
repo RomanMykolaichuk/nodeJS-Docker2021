d626fb772190bec276f2d1c7da6d15af583547b4
Roman Mykolaichuk <mykroman@ukr.net>
2021-10-01 17:22:43 +0300

    +

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
-const express = require("express")
-const cors = require("cors")
-const lowDb = require("lowdb")
-const FileSync = require("lowdb/adapters/FileSync")
-// const bodyParser = require("body-parser")
-const { nanoid } = require("nanoid")
+const http = require('http');
+const PORT =3300
 
-const db = lowDb(new FileSync('db.json'))
+const server = http.createServer((request,response)=>{
+  response.setHeader("Content-Type", "text/html")
+  response.end('<h1>Hello World!</h1>')
+});
 
-db.defaults({ notes: [] }).write()
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
+server.listen(PORT)
 
 
-app.use(express.static('public'));
-
-
-const PORT = 4000;
-
-app.get('/notes', (req, res) => {
-  const data = db.get("notes").value()
-  return res.json(data)
-})
-
-app.post('/notes/new', (req, res) => {
-  const note = req.body
-  db.get("notes").push({
-    ...note, id: nanoid()
-  }).write()
-  res.json({ success: true })
-})
-
-app.listen(PORT, ()=> {
-  console.log(`Backend is running on http://localhost:${PORT}`)
-})
